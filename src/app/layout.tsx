7fcc9bee5f91bf8e3d7ddcd876e0ae9dd45c3750
Justin B. Watson <justin@watsonnetworks.com>
2025-10-19 07:33:59 -0700

    include element lol

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
       <head />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
